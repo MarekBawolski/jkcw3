4a252446786cf6435a4237f008b856e68eadac7a
s434655 <s434655@lts.wmi.amu.edu.pl>
2018-07-19 18:14:19 +0200

    characterstring

---
+++ src/Calculator.java
@@ -0,0 +1,38 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author s434655
+ */
+public class Calculator {
+
+
+    public Calculator() {
+    }
+    
+    public void start() {
+        while (true) {
+            System.out.print("command: ");
+            String command = reader.readString();
+            if (command.equals("end")) {
+                break;
+            }
+
+            if (command.equals("sum")) {
+                sum();
+            } else if (command.equals("difference")) {
+                difference();
+            } else if (command.equals("product")) {
+                product();
+            }
+        }
+
+        statistics();
+    }
+}
